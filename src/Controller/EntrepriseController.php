19bd3a3123a18ceda582a431c5c116b5d231bff5
TaniaBar <tania.barillaro1@gmail.com>
2024-10-21 16:39:30 +0200

    added form to contact company, template and edit entrepriseController

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Entreprise;
 use App\Entity\Produit;
+use App\Form\ContactType;
 use App\Repository\ProduitRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/entreprise', name: 'app_entreprise_')]
@@ -44,5 +48,42 @@ class EntrepriseController extends AbstractController
             'produits' => $entrepriseProduits,
             'entreprise' => $entreprise,
         ]);
+    }
+
+    #[Route('/{slug}/contacter', name: 'contact')]
+    public function contacterEntreprise(string $slug, Request $request, MailerInterface $mailer, EntityManagerInterface $em): Response
+    {
+        $entreprise = $em->getRepository(Entreprise::class)->findOneBy(['slug' => $slug]);
+
+        if(!$entreprise) {
+            throw $this->createNotFoundException('Entreprise introuvable!');
+        }
+        
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $email = (new Email())
+            ->from($data['email'])
+            ->to($entreprise->getEmailEntreprise())
+            ->subject('Nouveau message')
+            ->text($data['message']);
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Ton message à bien été envoyé!');
+
+            return $this->redirectToRoute('app_entreprise_index');
+        }
+
+        return $this->render('entreprise/contact.html.twig', [
+            'entreprise' => $entreprise,
+            'form' => $form->createView(),
+        ]);
+
+        
+
     }
 }
