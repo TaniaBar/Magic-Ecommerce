9f4ac61529011f7f35a84d42b009bdfcab4b9774
TaniaBar <tania.barillaro1@gmail.com>
2024-10-24 16:09:05 +0200

    edit company style

--- assets/app.js
+++ assets/app.js
@@ -15,17 +15,26 @@ import './styles/produit.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
-document.addEventListener('DOMContentLoaded', function() {
-    const roleSelectors = document.querySelectorAll('.role-selector');
-    const companyFields = document.getElementById('company-fields');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const roleSelectors = document.querySelectorAll('.role-selector');
+//     const companyFields = document.getElementById('company-fields');
 
-    roleSelectors.forEach(function (roleSelector) {
-        roleSelector.addEventListener('change', function() {
-            if (this.value === 'ROLE_ENTREPRISE') {
-                companyFields.style.display = 'block';
-            } else {
-                companyFields.style.display = 'none';
-            }
-        });
+//     roleSelectors.forEach(function (roleSelector) {
+//         roleSelector.addEventListener('change', function() {
+//             if (this.value === 'ROLE_ENTREPRISE') {
+//                 companyFields.style.display = 'block';
+//             } else {
+//                 companyFields.style.display = 'none';
+//             }
+//         });
+//     });
+// })
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('#navbarSupportedContent');
+    
+    navbarToggler.addEventListener('click', () => {
+        navbarCollapse.classList.toggle('show');
     });
-})
+});
